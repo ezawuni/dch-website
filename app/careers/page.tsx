57eadc43b2471ae9000bd04ea3bb74e52932f7a9
Emmanuel Awuni <ezawuni@gmail.com>
2025-07-26 23:18:17 +0000

    Fix: add default export to careers page

---
+++ app/careers/page.tsx
@@ -0,0 +1,8 @@
+export default function Careers() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Careers</h1>
+      <p>We’re always looking for talented people. Check back soon for openings!</p>
+    </main>
+  );
+}
